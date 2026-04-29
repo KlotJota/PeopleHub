120675b3f6bdee9950ae3ebfdd644c7c13735533
KlotJota <jonathan.specamillio@hotmail.com>
2026-04-28 23:08:30 -0300

    chore: estrutura inicial do projeto usando .NET 10, docker e EF core

---
+++ PeopleHub.Web/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PeopleHub.Web.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Person>()
+            .HasIndex(p => p.Cpf)
+            .IsUnique();
+    }
+}
---
+++ PeopleHub.Web/Program.cs
@@ -0,0 +1,42 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")?
+    .Replace("${DB_SERVER}", Environment.GetEnvironmentVariable("DB_SERVER"))
+    .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
+    .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+    .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
